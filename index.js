9fc46d8b8505ce731b6ceea13c29fd7780cd3bbe
Jaruba Firanelli <jaruba@Jarubas-MacBook-Pro.local>
2018-07-16 21:56:15 +0300

    Support for `torrent-worker`

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var Tracker = require('bittorrent-tracker/client')
 inherits(Discovery, EventEmitter)
 
 function Discovery (opts) {
+
   var self = this
   if (!(self instanceof Discovery)) return new Discovery(opts)
   EventEmitter.call(self)
@@ -18,6 +19,12 @@ function Discovery (opts) {
   if (!opts.peerId) throw new Error('Option `peerId` is required')
   if (!opts.infoHash) throw new Error('Option `infoHash` is required')
   if (!process.browser && !opts.port) throw new Error('Option `port` is required')
+
+  self.noSeeding = opts && opts.noSeeding
+
+  self.niceSeeder = false;
+  self.whenSeeder = 0;
+  self.seedDelay = 0;
 
   self.peerId = typeof opts.peerId === 'string'
     ? opts.peerId
@@ -45,10 +52,10 @@ function Discovery (opts) {
   }
   self._onDHTPeer = function (peer, infoHash) {
     if (infoHash.toString('hex') !== self.infoHash) return
-    self.emit('peer', peer.host + ':' + peer.port, 'dht')
+    self.emit('peer', { address: peer.host + ':' + peer.port, force: (self.amSeeder && !self.noSeeding) }, 'dht')
   }
   self._onTrackerPeer = function (peer) {
-    self.emit('peer', peer, 'tracker')
+    self.emit('peer', { address: peer, force: (self.amSeeder && !self.noSeeding) }, 'tracker')
   }
   self._onTrackerAnnounce = function () {
     self.emit('trackerAnnounce')
@@ -103,16 +110,65 @@ Discovery.prototype.updatePort = function (port) {
   }
 }
 
-Discovery.prototype.complete = function (opts) {
-  if (this.tracker) {
-    this.tracker.complete(opts)
+Discovery.prototype.setTorrent = function () {
+  
+}
+
+Discovery.prototype.seedCheck = function () {
+  this.complete();
+  var changed = false
+  if (this.seedDelay == 180000 && this.whenSeeder < Date.now() - 600000) {
+    // 10 min passed => 5 min interval
+    this.seedDelay = 300000;
+    changed = true;
+  } else if (this.seedDelay == 300000 && this.whenSeeder < Date.now() - 1800000) {
+    // 30 min passed => 10 min interval
+    this.seedDelay = 600000;
+    changed = true;
+  } else if (this.seedDelay == 600000 && this.whenSeeder < Date.now() - 3600000) {
+    // 60 min passed => 15 min interval
+    this.seedDelay = 900000;
+    changed = true;
+  } else if (this.seedDelay == 600000 && this.whenSeeder < Date.now() - 7200000) {
+    // 120 min passed => 30 min interval
+    this.seedDelay = 1800000;
+    changed = true;
   }
+  if (changed) {
+    clearInterval(this.niceSeeder);
+    this.niceSeeder = setInterval(this.seedCheck, this.seedDelay);
+  }
+};
+
+Discovery.prototype.complete = function (opts) {
+  this.amSeeder = true;
+
+  if (!this.noSeeding) {
+    if (this.tracker) {
+      if (!this.niceSeeder) {
+        this.whenSeeder = Date.now();
+        this.seedDelay = 180000; // start at a 3 min interval
+        this.niceSeeder = setInterval(this.seedCheck, this.seedDelay);
+      }
+      this.tracker.complete(opts)
+    }
+  }
+}
+
+Discovery.prototype.update = function (opts) {
+  if (this.tracker)
+    this.tracker.update(opts);
 }
 
 Discovery.prototype.destroy = function (cb) {
   var self = this
   if (self.destroyed) return
   self.destroyed = true
+
+  if (self.niceSeeder) {
+    clearInterval(self.niceSeeder);
+    self.niceSeeder = false;
+  }
 
   clearTimeout(self._dhtTimeout)
 
